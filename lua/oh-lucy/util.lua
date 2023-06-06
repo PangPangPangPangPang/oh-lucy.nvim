bf42cb3fa15a305fcdeb8055ede4670ec812e6b0
wangyefeng <wangyefeng@bytedance.com>
2023-06-06 19:39:53 +0800

    update

--- lua/oh-lucy/util.lua
+++ lua/oh-lucy/util.lua
@@ -1,23 +1,30 @@
 local M = {}
 
- local function highlight(group, properties)
-   local bg = properties.bg == nil and '' or 'guibg=' .. properties.bg
-   local fg = properties.fg == nil and '' or 'guifg=' .. properties.fg
-   local style = properties.style == nil and '' or 'gui=' .. properties.style
-   local cmd = table.concat({ 'highlight', group, bg, fg, style }, ' ')
-   vim.api.nvim_command(cmd)
- end
+local function highlight(group, properties)
+        if properties.link ~= nil then
+                local cmd = table.concat({ 'hi default link', group, properties.link }, ' ')
+                vim.api.nvim_command(cmd)
+                return
+        end
 
- function M.load(theme)
-   if vim.g.colors_name then
-     vim.cmd 'hi clear'
-   end
-   vim.o.termguicolors = true
-   for _, v in pairs(theme) do
-     for group, properties in pairs(v) do
-       highlight(group, properties)
-     end
-   end
- end
+        local bg = properties.bg == nil and '' or 'guibg=' .. properties.bg
+        local fg = properties.fg == nil and '' or 'guifg=' .. properties.fg
+        local style = properties.style == nil and '' or 'gui=' .. properties.style
+        local cmd = table.concat({ 'highlight', group, bg, fg, style }, ' ')
+        vim.api.nvim_command(cmd)
+end
 
- return M
+function M.load(theme)
+        if vim.g.colors_name then
+                vim.cmd 'hi clear'
+        end
+        vim.o.termguicolors = true
+        for _, v in pairs(theme) do
+                for group, properties in pairs(v) do
+                        highlight(group, properties)
+                end
+        end
+end
+
+return M
+
